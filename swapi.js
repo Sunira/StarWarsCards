d9f4bdd3542777d15b7456be3c6188aa4919ce41
Sunira Moses <sunira.moses@gmail.com>
2020-05-13 22:27:25 -0400

    Nicer Fonts, More Subtle Slider

--- swapi.js
+++ swapi.js
@@ -1,11 +1,9 @@
 var swapi = {
-  init: function () {
-    this.loadCharacters();
-  },
   characterCount: "",
   setupRangeInput: function () {
     const range = document.getElementById("range");
     const rangeV = document.getElementById("rangeV");
+
     function setValue() {
       range.value = range.value || 10;
       const newValue = Number(
@@ -15,53 +13,55 @@ var swapi = {
       rangeV.innerHTML = `<span>${range.value}</span>`;
       rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
 
-      // Any time range value is set, collect new SWAPI Chars
+      // Any time range value is set, collect new SWAPI Chars and clear Old Ones
       document.querySelector("#swapi-cards").innerHTML = "";
       swapi.getRandomCharacters(range.value);
     }
-    document.addEventListener("DOMContentLoaded", setValue);
-    range.value = 10;
+    document.addEventListener("DOMContentLoaded", setValue());
+    range.value = 5;
     range.addEventListener("change", setValue);
   },
+
+  //Retrieve count of character records
   loadCharacters: async function () {
     const response = await fetch("http://swapi.dev/api/people/");
     const allSWAPIChars = await response.json();
     this.characterCount = allSWAPIChars.count || 0;
-
-    this.getRandomCharacters(10);
     this.setupRangeInput();
   },
 
+  //Create array of UNIQUE character IDs to render
   getRandomCharacters: function (numChars) {
-    //Create array of UNIQUE character IDs to render
     var idArray = [];
-
     while (idArray.length < numChars) {
       var rando = Math.floor(Math.random() * swapi.characterCount) + 1;
       if (idArray.indexOf(rando) === -1) idArray.push(rando); // Only push unique character Ids.
     }
-
+    //Get Information for Each of these Character IDs
     idArray.forEach(function (value) {
-      swapi.loadCharacter(value);
+      swapi.getSingleCharacter(value);
     });
   },
 
-  loadCharacter: async function (characterID) {
+  // Hit individual Character Endpoint
+  getSingleCharacter: async function (characterID) {
     const response = await fetch("http://swapi.dev/api/people/" + characterID);
     const swapiChar = await response.json();
     this.createCharacterCard(swapiChar, characterID);
-    console.log(swapiChar);
   },
+
+  //Generate HTML for each
   createCharacterCard(swapiChar, characterID) {
     const markup = `
                   <div class="person" style="background-image:url(https://source.unsplash.com/800x5${characterID}/?space,stars)">
-                      <h2 cass="swapi-name">
+                      <h2 class="swapi-name">
                           ${swapiChar.name}
                       </h2>
                       <div class="swapi-content">
-                      <div class="swapi-item"><p class="swapi-prop"> Skin Color </p> <p class="swapi-val">${swapiChar.skin_color}</div>
+                      <div class="swapi-item"><p class="swapi-prop"> Gender </p> <p class="swapi-val">${swapiChar.gender}</div>
                       <div class="swapi-item"><p class="swapi-prop"> Hair Color </p>  <p class="swapi-val">${swapiChar.hair_color}</div>
                       <div class="swapi-item"><p class="swapi-prop"> Skin Color </p> <p class="swapi-val">${swapiChar.skin_color}</div>
+                      <div class="swapi-item"><p class="swapi-prop"> Eye Color </p> <p class="swapi-val">${swapiChar.eye_color}</div>
                       </div>
                   </div>
                   `;
@@ -71,4 +71,4 @@ var swapi = {
   },
 };
 
-swapi.init();
+swapi.loadCharacters();
